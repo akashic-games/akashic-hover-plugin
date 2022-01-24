3fac12a66bd7cc40f050ce3c0b4f0847d72d48c6
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-01-24 13:06:39 +0900

    Mod HoverPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { Converter } from "./Converter";
 export { HoverableE } from "./HoverableE";
-export { HoverPlugin } from "./HoverPluginImpl";
+
+// HoverPlugin.ts で module.exports しているため、そのまま export すると使用側で型がおかしくなる。
+// 後方互換性のため module.exports は残しここでキャストしている。
+import * as plugin from "./HoverPlugin";
+const hoverPlugin = plugin as g.OperationPluginStatic;
+export { hoverPlugin as HoverPlugin };
